0bd5f7293bb77ea22b164cb1a9a0e7cabd92b272
Akas_Datta <012akasdatta@gmail.com>
2023-06-19 20:19:00 +0600

    chef added on this recipepage and other component modified

--- src/pages/Shared/RecipePage/RecipePage.jsx
+++ src/pages/Shared/RecipePage/RecipePage.jsx
@@ -35,11 +35,37 @@ const RecipePage = () => {
   return (
     <div>
       <ToastContainer />
+        <div className="mt-5 pt-5">
+        <div className="h-100 container">
+          <Row className="d-flex justify-content-center align-items-center h-100">
+            <Col xl={10}>
+              <Card className='border-0 '>
+                <Row className="g-0">
+                  <Col md={5} lg={5} className="d-flex align-items-center">
+                    <Card.Body className="p-4 p-lg-5 text-black">
+                      <h5 className="card-title">{category.name}</h5>
+                      <p className="card-text"><b>Experience:</b> {category.experence}</p>
+                      <p className="card-text"><b>Total likes:</b> {category.likes}</p>
+                      <p className="card-text"><b>Recipe number: {recipes.length}</b></p>
+                      <p className="card-text"><b>Descriptions:</b> {category.bio}</p>
+                    </Card.Body>
+                  </Col>
+                  <Col md={7} lg={7} className="d-none d-md-block">
+                    <Card.Img src={category.picture} alt="login form" className="img-fluid" style={{ borderRadius: '0 1rem 0 1rem' }} />
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
+          </Row>
+      </div>
+    </div>
+
+
       {recipes.map((recipe, index) => (
         <div key={recipe.id}>
           <div className='recipe'>
             <Card className="mb-5 py-5 container">
-              <Row  className="g-5">
+              <Row className="g-5">
                 <Col md={4}>
                   <Card.Img src={recipe.picture} className="img-fluid rounded-start" alt="..." />
                 </Col>
@@ -47,20 +73,25 @@ const RecipePage = () => {
                   <Card.Body>
                     <Card.Title>{recipe.name}</Card.Title>
                     <Card.Text>
-                      <b>Ingredients: </b>{recipe.making_details}
+                      <b>Ingredients: </b>
+                      {recipe.making_details}
                     </Card.Text>
                     <Card.Text>
-                      <p>Ratings: <Rating
-                        placeholderRating={recipe.rating}
-                        readonly
-                        emptySymbol={<FaRegStar></FaRegStar>}
-                        placeholderSymbol={<FaStar className='text-danger'></FaStar>}
-                        fullSymbol={<FaStar></FaStar>}
-                      ></Rating> {recipe.rating}</p>
+                      <p>
+                        Ratings:{' '}
+                        <Rating
+                          placeholderRating={recipe.rating}
+                          readonly
+                          emptySymbol={<FaRegStar></FaRegStar>}
+                          placeholderSymbol={<FaStar className='text-danger'></FaStar>}
+                          fullSymbol={<FaStar></FaStar>}
+                        ></Rating>{' '}
+                        {recipe.rating}
+                      </p>
 
-                      <Button 
-                        variant="success" 
-                        onClick={() => handleFavoriteClick(index)} 
+                      <Button
+                        variant="success"
+                        onClick={() => handleFavoriteClick(index)}
                         disabled={favoriteStates[index]}
                       >
                         {favoriteStates[index] ? 'Favorited' : 'Add to Favorites'}
